3ec77e3ad9683121d19d3e90491835e4a81a175e
luca-chierchia <147411030+luca-chierchia@users.noreply.github.com>
2023-10-19 12:42:01 +0200

    Add files via upload

---
+++ gen_triangolo_iscoscele/triangolo_isoscele.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace  std;
+
+int main() {
+
+    int h;
+    int i=0;
+    int k=0;
+    do{
+        cout<<"Inserisci altezza >0:"<<endl;
+        cin>>h;
+    }while(h<=0);
+
+    while(h>i) {
+
+        for (int j = 0; j < h; j++) {
+            if (i + j >= h - 1)
+                cout << '*';
+            else
+                cout << " ";
+        }
+        cout<<endl;
+        i++;
+
+        if(i==h-1)
+            break;
+    }
+    while(h>k) {
+
+        for (int j = 0; j < h; j++) {
+            if (k - j <=0)
+                cout << '*';
+            else
+                cout << " ";
+        }
+        cout<<endl;
+        k++;
+    }
+
+
+    return 0;
+    }
+
+
+
